817be88b7fcd58aba2f4b22faf501e376211d0b8
Emily Yau <emily.yau@education.gov.uk>
2024-03-19 11:40:38 +0000

    adding initial toggle radio button for stats neighbours chart

--- R/enabler2_page.R
+++ R/enabler2_page.R
@@ -186,6 +186,12 @@ enabler2_tab <- function() {
                   ),
                   gov_row(
                     h2("Turnover rates by local authority"),
+                    radioGroupButtons(
+                      "turnover_stats_toggle",
+                      label = NULL,
+                      choices = c("All local authorities", "10 Statistical Neighbours"),
+                      selected = "All local authorities"
+                    ),
                     p("This chart is reactive to the Local Authority and Regional filters at the top and will not react to the National filter. The chart will display all Local Authorities overall or every Local Authority in the selected Region."),
                     p(sprintf("The graph represents data from %s.", max(workforce_data$time_period))),
                     br(),
