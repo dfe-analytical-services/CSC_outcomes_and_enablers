8225b555dd37ef75ec8965ede0caa374a005d808
cjrace <camrace8@gmail.com>
2024-04-04 16:21:33 +0100

    removing screenshots from shinytest2

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -5,7 +5,8 @@ test_that("{shinytest2} recording: CSC_outcomes_and_enablers_error_test", {
     timeout = 25 * 1000,
     wait = TRUE,
     height = 886, width = 1203,
-    variant = NULL
+    variant = NULL,
+    expect_values_screenshot_args = FALSE
   )
   app$set_inputs(navlistPanel = "outcome1_page")
   app$set_window_size(width = 1203, height = 886)
@@ -14,5 +15,4 @@ test_that("{shinytest2} recording: CSC_outcomes_and_enablers_error_test", {
   app$set_inputs(geographic_breakdown_o1 = "East Midlands")
   app$wait_for_idle(500)
   app$expect_values()
-  app$expect_screenshot()
 })
