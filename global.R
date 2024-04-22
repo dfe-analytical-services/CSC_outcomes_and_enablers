c69978d2b99f39ab96a4740322bd1c5b686cd98a
Emily Yau <emily.yau@education.gov.uk>
2024-04-22 10:29:28 +0100

    Updating EFH list and test scripts

--- global.R
+++ global.R
@@ -130,7 +130,9 @@ af_child_abuse_extra_filter <- assessment_factors %>%
   filter(str_detect(assessment_factor, "Abuse|abuse|Neglect|neglect")) %>%
   select(assessment_factor) %>%
   pull("assessment_factor")
-extra_familial_harm_af <- c("Alcohol Misuse child", "Drug Misuse child", "Missing", "Child sexual exploitation", "Trafficking", "Gangs", "Child criminal exploitation")
+
+extra_familial_harm_af <- c("Missing", "Child sexual exploitation", "Trafficking", "Gangs", "Child criminal exploitation")
+# "Alcohol Misuse child", "Drug Misuse child", "Missing", "Child sexual exploitation", "Trafficking", "Gangs", "Child criminal exploitation"
 
 # Read in stats neighbours
 stats_neighbours <- head(statistical_neighbours(), 152)
