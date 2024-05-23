646520c020525d87421f71ab9fa72e6044834fd8
Emily Yau <emily.yau@education.gov.uk>
2024-05-23 09:03:24 +0100

    Layout for enabler3

--- R/enabler3_page.R
+++ R/enabler3_page.R
@@ -13,15 +13,59 @@ enabler3_tab <- function() {
         div(
           class = "input_box",
           style = "min-height:100%; height = 100%; overflow-y: visible",
-          gov_row(
-            column(
-              width = 6,
-              h2("Inputs go here")
+          layout_columns(
+            selectizeInput(
+              inputId = "select_geography_e3",
+              label = "Select a geographical level:",
+              # Change this to look at the relevant dataset for outcome 4
+              choices = unique(placement_data %>% pull("geographic_level")),
+              selected = NULL,
+              multiple = FALSE,
+              options = NULL
             ),
-            column(
-              width = 6,
-              h2("Inputs go here")
-            )
+            conditionalPanel(
+              condition = "input.select_geography_e3 != 'National'",
+              selectizeInput(
+                inputId = "geographic_breakdown_e3",
+                label = "Select a location: ",
+                choices = NULL,
+                selected = NULL,
+                multiple = FALSE,
+                options = NULL
+              )
+            ),
+            col_widths = c(4, 8)
+          ),
+          layout_columns(
+            conditionalPanel(
+              condition = "input.select_geography_e3 != 'National'",
+              column(
+                width = 3,
+                checkbox_Input(
+                  inputId = "national_comparison_checkbox_e3",
+                  cb_labels = "Compare with National",
+                  checkboxIds = "Yes_national_e3",
+                  label = "",
+                  hint_label = NULL,
+                  small = TRUE
+                )
+              )
+            ),
+            conditionalPanel(
+              condition = "(input.select_geography_o4 == 'Local authority')",
+              column(
+                width = 3,
+                checkbox_Input(
+                  inputId = "region_comparison_checkbox_e3",
+                  cb_labels = "Compare with Region",
+                  checkboxIds = "Yes_region_e3",
+                  label = "",
+                  hint_label = NULL,
+                  small = TRUE
+                )
+              )
+            ),
+            col_widths = c(4, 8)
           )
         )
       ),
@@ -30,20 +74,124 @@ enabler3_tab <- function() {
         br(),
         h2("Confirmation Sentence"),
         br(),
+      ),
+      gov_row(
         div(
           tabsetPanel(
-            id = "outcome3_panels",
+            id = "enabler3_panels",
             type = "tabs",
             tabPanel(
               "Spending",
               fluidRow(
-                p("testing")
+                br(),
+              ),
+              fluidRow(
+                column(
+                  width = 6,
+                  value_box(
+                    title = "title",
+                    value = "value"
+                  )
+                ),
+                column(
+                  width = 6,
+                  value_box(
+                    title = "title",
+                    value = "value"
+                  )
+                ),
+                br(),
+                p("Rationale here")
+              ),
+              accordion(
+                accordion_panel(
+                  "Accordion one",
+                  gov_row(
+                    h2("Share of local authority total spend on Children’s Services, and per child spend."),
+                    #
+                    #
+                    br(),
+                    plotlyOutput(""),
+                    br(),
+                    # details(
+                    #   inputId = "",
+                    #   label = "",
+                    #   help_text = (p(
+                    #
+                    #   ))
+                    # )
+                  ),
+                  gov_row(
+                    h2("by region"),
+                  ),
+                  gov_row(
+                    h2("by la"),
+                    # p(),
+                    radioGroupButtons(
+                      "spending1_stats_toggle",
+                      label = NULL,
+                      choices = c("All local authorities", "10 Statistical Neighbours"),
+                      selected = "All local authorities"
+                    ),
+                    # uiOutput("SN_sgo"),
+                  )
+                ),
+                accordion_panel(
+                  "Share of Children and Young People Services spend minus spend on CLA",
+                  gov_row(
+                    h2("time series")
+                  ),
+                  gov_row(
+                    h2("by region")
+                  ),
+                  gov_row(
+                    h2("by la"),
+                    radioGroupButtons(
+                      "spending2_stats_toggle",
+                      label = NULL,
+                      choices = c("All local authorities", "10 Statistical Neighbours"),
+                      selected = "All local authorities"
+                    ),
+                  )
+                )
               )
             ),
             tabPanel(
               "Culture focused on outcomes from children and families and continually improving services",
               fluidRow(
-                p("testing")
+                # p("testing")
+              ),
+              fluidRow(
+                column(
+                  width = 6,
+                  value_box(
+                    title = "title",
+                    value = "value"
+                  )
+                ),
+                br(),
+                p("Rationale"),
+              ),
+              accordion(
+                accordion_panel(
+                  "Culture focused on outcomes from children and families and continually improving services",
+                  gov_row(
+                    h2("Ofsted – The impact of leaders on social work practice with children and families"),
+                  ),
+                  gov_row(
+                    h2("by region"),
+                  ),
+                  gov_row(
+                    h2("by la"),
+                    radioGroupButtons(
+                      "ofted_stats_toggle",
+                      label = NULL,
+                      choices = c("All local authorities", "10 Statistical Neighbours"),
+                      selected = "All local authorities"
+                    ),
+                    # uiOutput("SN_sgo"),
+                  )
+                )
               )
             )
           )
