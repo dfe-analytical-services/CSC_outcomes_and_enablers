babe5dcc5d1a09f3fb0fc842357d415b797755f3
Rich Bielby <richard.bielby@education.gov.uk>
2024-07-05 12:33:55 +0100

    Added manifest.joson build to commit hooks

--- global.R
+++ global.R
@@ -10,6 +10,8 @@
 
 # Library calls ---------------------------------------------------------------------------------
 shhh <- suppressPackageStartupMessages # It's a library, so shhh!
+shhh(library(git2r))
+shhh(library(rsconnect))
 shhh(library(shiny))
 shhh(library(shinyjs))
 shhh(library(tools))
