564a8ad63aa7990c00a6de3716ff8be74692e5a7
PICKERING <Andre.PICKERING@EDUCATION.GOV.UK>
2024-10-23 15:58:22 +0100

    new ofsted data

--- R/read_data.R
+++ R/read_data.R
@@ -682,9 +682,15 @@ read_spending_data2 <- function(file = "data/RO3_2023-24_data_by_LA.ods") {
 }
 
 # Ofsted leadership data
-read_ofsted_leadership_data <- function(file = "data/Childrens_social_care_in_England_2023_underlying_data.ods") {
+# read_ofsted_leadership_data <- function(file = "data/Childrens_social_care_in_England_2023_underlying_data.ods") {
+read_ofsted_leadership_data <- function(file = "data/LA_Inspection_Outcomes_as_at_March_2024.ods") {
   # Import data and drop top 3 rows to ensure headers are correct
-  ofsted_leadership_data <- read_ods(file, sheet = "LA_level_at_31_Mar_2023", skip = 3)
+  # ofsted_leadership_data <- read_ods(file, sheet = "LA_level_at_31_Mar_2023", skip = 3)
+  ofsted_leadership_data <- read_ods(file, sheet = "Inspections_31_March_2024", skip = 2)
+
+  # Remove authorities that aren't yet inspected
+  ofsted_leadership_data <- ofsted_leadership_data %>%
+    filter(`Inspection date` != "Not yet inspected")
 
   # Convert "Inspection date" column to date format and copy the year into new "time_period" column
   ofsted_leadership_data$`Inspection date` <- as.Date(ofsted_leadership_data$`Inspection date`, format = "%d/%m/%Y")
@@ -740,6 +746,8 @@ read_ofsted_leadership_data <- function(file = "data/Childrens_social_care_in_En
       outstanding_count = sum(outstanding_count)
     )
 
+  max(ofsted_leadership_data$time_period)
+
   region_counts$time_period <- max(ofsted_leadership_data$time_period)
 
   region_counts$geographic_level <- "Regional"
@@ -756,6 +764,8 @@ read_ofsted_leadership_data <- function(file = "data/Childrens_social_care_in_En
       outstanding_count = sum(outstanding_count)
     )
 
+
+
   # Combine the new data with the existing data
   ofsted_leadership_data <- bind_rows(ofsted_leadership_data, region_counts, national_counts)
 
@@ -764,6 +774,7 @@ read_ofsted_leadership_data <- function(file = "data/Childrens_social_care_in_En
 
   return(ofsted_leadership_data)
 }
+
 
 pivot_ofsted_data <- function() {
   # Pivoted version
